8db8f591ed85f46aa1af55f31278f2e4b123743a
Aluno <Aluno@UPGT-SPBR-PC153>
2025-04-29 10:04:39 -0300

    Adicionar arquivos de projeto.

---
+++ WebAppPiagetRafaela/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAppPiagetRafaela.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
